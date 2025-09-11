b9796829800c1576f767e6e6e358060a0ad2f914
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-09-11 12:29:00 -0300

    feat: add CategoryController

---
+++ backend/src/main/java/com/dscatalog/api/controller/CategoryController.java
@@ -0,0 +1,50 @@
+package com.dscatalog.api.controller;
+
+import com.dscatalog.api.dto.CategoryRequestDTO;
+import com.dscatalog.api.dto.CategoryResponseDTO;
+import com.dscatalog.api.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<Page<CategoryResponseDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponseDTO> create(@RequestBody CategoryRequestDTO dto) {
+        CategoryResponseDTO response = service.create(dto);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(response.id()).toUri();
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponseDTO> update(@PathVariable Long id, @RequestBody CategoryRequestDTO dto) {
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
