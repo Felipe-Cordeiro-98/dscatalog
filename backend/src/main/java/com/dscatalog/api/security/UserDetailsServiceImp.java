c454efb56fe85a40575ebd301c919d77dd941e45
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-09-12 13:39:24 -0300

    feat: add UserDetailsServiceImp

---
+++ backend/src/main/java/com/dscatalog/api/security/UserDetailsServiceImp.java
@@ -0,0 +1,21 @@
+package com.dscatalog.api.security;
+
+import com.dscatalog.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+    }
+}
