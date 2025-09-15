cd2ee2d9c1b36c66bcf496738582a49ddaefa765
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-09-15 10:34:45 -0300

    feat: add LayoutPages component for main layout structure

---
+++ frontend/src/Layouts/LayoutPages.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+
+export default function LayoutPages() {
+    return (
+        <>
+            <Header />
+            <main className="w-full h-[calc(100dvh-60px)] p-5 bg-[#F2F2F2]">
+                <Outlet />
+            </main>
+        </>
+    );
+}
