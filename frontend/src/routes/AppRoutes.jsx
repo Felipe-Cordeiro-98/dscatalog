9ef8f8965ab57eb6ff5c11112a93b3ec366e357d
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-09-18 20:03:53 -0300

    feat: add route to Catalog page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LayoutPages from "../Layouts/LayoutPages";
 import Home from "../pages/Home";
+import Catalog from "../pages/Catalog";
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route element={<LayoutPages />}>
                 <Route path="/" element={<Home />} />
+                <Route path="/catalog" element={<Catalog />} />
             </Route>
         </Routes>
     );
