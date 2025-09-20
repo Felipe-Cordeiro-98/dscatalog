675bfb9234008c57f00967472a1c18283e68407c
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-09-19 21:11:17 -0300

    feat: add productService

---
+++ frontend/src/services/productService.js
@@ -0,0 +1,28 @@
+import api from "./api";
+
+const API_URL = "/products";
+
+export const getProducts = async () => {
+    const response = await api.get(API_URL);
+    return response.data;
+};
+
+export const getProductById = async (id) => {
+    const response = await api.get(`${API_URL}/${id}`);
+    return response.data;
+};
+
+export const createProduct = async (product) => {
+    const response = await api.post(API_URL, product);
+    return response.data;
+};
+
+export const updateProduct = async (id, product) => {
+    const response = await api.put(`${API_URL}/${id}`, product);
+    return response.data;
+};
+
+export const deleteProduct = async (id) => {
+    const response = await api.delete(`${API_URL}/${id}`);
+    return response.data;
+};
