f3ba3c673edc34ca5bb9c5aa2b7fc3088b94ac81
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-10-14 12:53:35 -0300

    feat: add footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="bg-[#407BFF] text-white p-3 text-center text-base tracking-wide">
+            <p>
+                Desenvolvido por <strong>Felipe Cordeiro</strong> &copy; {new Date().getFullYear()}
+            </p>
+        </footer>
+    );
+}
