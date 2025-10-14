a3516b8e1d6c847801669485585f9c3d92531afb
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-10-14 12:55:59 -0300

    feat: add PublicLayout component with Header and Footer

--- frontend/src/Layouts/PublicLayout.jsx
+++ frontend/src/Layouts/PublicLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-export default function LayoutPages() {
+export default function PublicLayout() {
     return (
         <>
             <Header />
-            <main className="w-full h-[calc(100dvh-60px)] p-5">
+
+            <main className="w-full min-h-[calc(100dvh-108px)] p-5 relative">
                 <Outlet />
             </main>
+            
+            <Footer />
         </>
     );
 }
